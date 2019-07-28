71afaf95edcac673556c481eda114dd90bbd6ed8
soyun22 <2002airplane4@gmail.com>
2019-07-28 22:34:50 +0900

    프로젝트 시작

--- pirodjangogirls/manage.py
+++ pirodjangogirls/manage.py
@@ -5,7 +5,7 @@ import sys
 
 
 def main():
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'priodjangogirls.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pirodjangogirls.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
--- pirodjangogirls/pirodjangogirls/wsgi.py
+++ pirodjangogirls/pirodjangogirls/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for priodjangogirls project.
+WSGI config for pirodjangogirls project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'priodjangogirls.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'pirodjangogirls.settings')
 
 application = get_wsgi_application()
